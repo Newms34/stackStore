2eb36cd3b7e250828daa4f6683ea43b436d2d14f
Michael Bae <michaelbbae@gmail.com>
2015-03-03 17:06:44 -0500

    accessed database data for coffee and mint

---
+++ browser/js/productlisting/productfactory.js
@@ -0,0 +1,14 @@
+app.factory('ProductFactory', function ($http) {
+    return {
+        getCoffee: function() {
+            return $http.get('/api/coffee').then(function(response) {
+                return response.data;
+            })
+        },
+        getMints: function() {
+            return $http.get('api/mint').then(function(response) {
+                return response.data;
+            })
+        }
+    };
+});
--- browser/js/productlisting/productlisting.js
+++ browser/js/productlisting/productlisting.js
@@ -1,16 +1,30 @@
-// 'use strict';
-// app.config(function ($stateProvider) {
+'use strict';
+app.config(function ($stateProvider) {
 
-//     $stateProvider.state('home', {
-//         url: '/',
-//         controller: 'HomeCtrl',
-//         templateUrl: 'js/home/home.html'
-//     });
+    $stateProvider.state('coffee', {
+        url: '/coffee',
+        controller: 'ProductCtrl',
+        templateUrl: 'js/productlisting/productlisting.html'
+    });
 
-// });
+    $stateProvider.state('mint', {
+      url: '/mint',
+      controller: 'ProductCtrl',
+      templateUrl: 'js/productlisting/productlisting.html'
+    });
+});
 
-// app.controller('HomeCtrl', function ($scope) {
+app.controller('ProductCtrl', function ($scope, ProductFactory) {
 
+  $scope.getCoffeeProducts = function() {
+    ProductFactory.getCoffee().then(function(data) {
+      $scope.coffee = data;
+    });
+    ProductFactory.getMints().then(function(data) {
+      $scope.mint = data;
+    });
+  };
 
-
-// });
+  // $scope.getMintProducts = function() {
+  // };
+});
