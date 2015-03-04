6303e6e976207c6d9ba439514624c426bc31c569
Michael Bae <michaelbbae@gmail.com>
2015-03-04 18:02:47 -0500

    troubleshoot and fixed lint warnings

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -1,10 +1,10 @@
 'use strict';
-app.directive('navbar', function () {
-    return {
-        restrict: 'E',
-        scope: {
-          items: '='
-        }, 
-        templateUrl: 'js/common/directives/navbar/navbar.html'
-    };
+app.directive('navbar', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      items: '='
+    },
+    templateUrl: 'js/common/directives/navbar/navbar.html'
+  };
 });
