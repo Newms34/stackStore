ab238008f478514c59e82af51d2ca0140c511534
Michael Bae <michaelbbae@gmail.com>
2015-03-04 16:01:03 -0500

    product grid

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -6,23 +6,29 @@ var User = require('./user.js');
 var Product = require('./product.js');
 // var Product = require()
 var schema = new Schema({
-    // user reference should be object id http://mongoosejs.com/docs/api.html#schema-objectid-js
-    user:{type: Schema.Types.ObjectId, ref:'User'},
-    products:[{
-    	prodId:{type: Schema.Types.ObjectId, ref:'Product'},
-    	Price:Number,
-    	Quantity:Number
-    }],
-    status:String
+  // user reference should be object id http://mongoosejs.com/docs/api.html#schema-objectid-js
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  products: [{
+    prodId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    },
+    Price: Number,
+    Quantity: Number
+  }],
+  status: String
 });
 
 
 schema.pre('save', function(next) {
-    var ord = this;
-    if (ord.user!=='none' ){
-        //save in user's order history
-    }
-    next();
+  var ord = this;
+  if (ord.user !== 'none') {
+    //save in user's order history
+  }
+  next();
 });
 
 
