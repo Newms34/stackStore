e171a9e015a5029483ea164b8a65363c5a2e836f
Michael Bae <michaelbbae@gmail.com>
2015-03-05 16:49:51 -0500

    goodbye ng-init

--- browser/js/coffee/coffee-controller.js
+++ browser/js/coffee/coffee-controller.js
@@ -1,9 +1,6 @@
 'use strict';
 app.controller('CoffeeCtrl', function($scope, CoffeeFactory) {
-
-  $scope.showCoffee = function() {
-    CoffeeFactory.getCoffeeDb().then(function(data) {
-      $scope.coffee = data;
-    });
-  };
+  CoffeeFactory.getCoffeeDb().then(function(data) {
+    $scope.coffee = data;
+  });
 });
--- browser/js/mint/mint-controller.js
+++ browser/js/mint/mint-controller.js
@@ -1,9 +1,6 @@
 'use strict';
 app.controller('MintCtrl', function($scope, MintFactory) {
-
-  $scope.showMints = function() {
-    MintFactory.getMintsDb().then(function(data) {
-      $scope.mints = data;
-    });
-  };
+  MintFactory.getMintsDb().then(function(data) {
+    $scope.mints = data;
+  });
 });
