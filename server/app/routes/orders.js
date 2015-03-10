a707068daf461c37e0c7ea9f3e33a4dda92e5cde
Newms34 <Newms3450@msn.com>
2015-03-10 16:58:00 -0400

    orders full backend func

--- server/app/routes/orders.js
+++ server/app/routes/orders.js
@@ -11,16 +11,36 @@ router.post('/view', function(req, res) {
 router.post('/submit', function(req, res, next) {
     //record an order.
     var ordersToDb = req.body.ordersObj;
-    console.log('user name:',ordersToDb.user)
-    mongoose.model('User').find({email:ordersToDb.user},function(err,usr){
-        if (usr!==null){
+    console.log('user name:', ordersToDb.user)
+    mongoose.model('User').find({
+        email: ordersToDb.user
+    }, function(err, usr) {
+        if (usr !== null) {
             ordersToDb.user = usr[0]._id;
-            console.log(ordersToDb)
-            res.send('ordersToDb: ',ordersToDb)
-            mongoose.model('Order').create(ordersToDb);
-        }
-        else{
+            console.log('ordersToDb',ordersToDb)
+            mongoose.model('Order').create(ordersToDb,function(err,theOrd){
+                res.send(theOrd._id)
+            });
+            // var newOrder = mongoose.model('Order');
+            // mongoose
+            
+        } else {
             res.send('No User');
         }
     })
 });
+
+router.post('/payOrder', function(req, res, next) {
+    var ordId = req.body.id;
+    console.log('ordId: ', ordId)
+    var promoted = req.body.prom;
+    mongoose.model('Order').findById(ordId, function(err, theOrder) {
+        theOrder.status='paid';
+        if (promoted!=0){
+            theOrder.promoted=true;
+        }
+        console.log(theOrder);
+        theOrder.save()
+        res.send('Done');
+    })
+})
