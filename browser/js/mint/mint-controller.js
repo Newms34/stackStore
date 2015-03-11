7ba3cccb7a0fcf13735f662e395e7ec2c644439c
Newms34 <Newms3450@msn.com>
2015-03-10 21:27:38 -0400

    done

--- browser/js/mint/mint-controller.js
+++ browser/js/mint/mint-controller.js
@@ -4,12 +4,12 @@ app.controller('MintCtrl', function($scope, MintFactory, CoffeeFactory, addtocar
         $scope.things = data;
     });
 
-    $scope.getKitties = function(kitty) {
+     $scope.getKitties = function(kitty) {
         //search thru both dbs (on backend) and return concatenated arr
         CoffeeFactory.getByCatDb(kitty).then(function(data) {
-            $scope.things = data;
-        })
-    }
+            angular.copy(data, $scope.things);
+        });
+    };
 
     $scope.addtocart = function(id, type){
     	addtocart.addtocart(id, type);
